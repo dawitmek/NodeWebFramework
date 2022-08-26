3fa6191788f59db0ac26271b8eb271b4343dc909
Dawit Mengistie <65038000+dawitmek@users.noreply.github.com>
2022-08-26 17:20:31 -0400

    Delete params.js

--- params/params.js
+++
@@ -1,5 +0,0 @@
-const DATABASECONNECTION = 'mongodb+srv://admin:nodejs1234@cluster0.krlqmgt.mongodb.net/?retryWrites=true&w=majority'
-
-const SESSION_SECRET = "LRK:v2%MZ'm.qmEZp=e"
-
-module.exports = { DATABASECONNECTION, SESSION_SECRET }

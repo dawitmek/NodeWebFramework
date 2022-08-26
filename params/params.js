f1978f556e79cdad7ab7eb2f0677abad35d68f61
Dawit <dawitmekuria5@gmail.com>
2022-08-26 15:59:18 -0400

    added node modules | added engine in package.json

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = 'mongodb+srv://admin:nodejs1234@cluster0.krlqmgt.mongodb.net/?retryWrites=true&w=majority'
+
+const SESSION_SECRET = "LRK:v2%MZ'm.qmEZp=e"
+
+module.exports = { DATABASECONNECTION, SESSION_SECRET }
